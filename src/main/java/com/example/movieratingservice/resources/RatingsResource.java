2e474d4656fdd508c96293c7829be560666a004d
Victoria <jv.svedaite@gmail.com>
2020-04-29 22:39:34 -0500

    add UserRating object

--- src/main/java/com/example/movieratingservice/resources/RatingsResource.java
+++ src/main/java/com/example/movieratingservice/resources/RatingsResource.java
@@ -1,6 +1,7 @@
 package com.example.movieratingservice.resources;
 
 import com.example.movieratingservice.models.Rating;
+import com.example.movieratingservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,14 @@ public class RatingsResource {
     }
 
     @RequestMapping("users/{userId}")
-    public List<Rating> getUserRating(@PathVariable("userId") String userId){
+    public UserRating getUserRating(@PathVariable("userId") String userId){
                 //hardcode response from rating data api for now
                 List<Rating> ratings = Arrays.asList(
                         new Rating("1234", 4),
                         new Rating("5575", 5)
         );
-                return ratings;
+                UserRating userRating =  new UserRating();
+                userRating.setUserRating(ratings);
+                return userRating;
     }
 }
